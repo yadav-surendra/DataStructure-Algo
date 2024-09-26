9c5ff2223efe37a8643c4d9bb6c06411c5b7fb15
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-09-26 10:22:46 +0530

    problem solved

--- product_of_array_except_self.cpp
+++ product_of_array_except_self.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+/*
 // Brute force approach.
 int main()
 {
@@ -22,6 +23,34 @@ int main()
     for (int a:ans)
     {
         cout << a << " ";
+    }
+    return 0;
+}
+*/
+
+// optomized code
+int main()
+{
+    vector<int> vec = {1, 2, 3, 4};
+    vector<int> ans(4,1);
+   int n = vec.size();
+   // prefix calculation
+   for(int i=1; i<n; i++)
+   {
+    ans[i] = ans[i-1]*vec[i-1];
+   }
+
+    // for suffix
+    int suffix =1;
+    for(int i=n-2; i>=0; i--)
+    {
+     suffix *= vec[i+1]; 
+     ans[i] = suffix;
+    }
+
+    for(int val : ans)
+    {
+        cout<< val <<" ";
     }
     return 0;
 }
