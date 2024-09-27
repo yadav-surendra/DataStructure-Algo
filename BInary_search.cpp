66e553b0d58e06cd58d81ce33eff95cea1f58bdd
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-09-27 09:14:39 +0530

    code added for binary search

--- BInary_search.cpp
+++ BInary_search.cpp
@@ -1,3 +1,35 @@
 /*
 binary search is applicable only on sorted arrays.
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int BinarySearch(vector<int> arr , int target){
+    int n = arr.size();
+    int st = 0;
+    int end = n-1;
+    int mid ;
+    while(st<=end){
+        mid = (st+end)/2;
+        if(arr[mid]<target)
+        st = mid + 1;
+        else if(arr[mid]>target)
+        end = mid - 1;
+        else
+        return mid;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> vec = {1,3,6,8,12,15};
+    
+   int ans = BinarySearch(vec,12);
+   cout<< ans;
+    
+
+    
+}
