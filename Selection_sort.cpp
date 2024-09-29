deb9a7b50d9d2266742169ae75cafbb746ab367c
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-09-29 09:27:55 +0530

    improved logic

--- Selection_sort.cpp
+++ Selection_sort.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>  // For std::min
 using namespace std;
 
-int main(){
-    vector<int> vec = { 1,2 ,5, 8 ,1, 10, 14};
+int main() {
+    vector<int> vec = {1, 2, 1, 5, 8, 10, 14};
     int size = vec.size();
-    int i=0 ;
-    int minimum = INT16_MAX;
-    while(i<size)
-    {
-        for(int j=i; j<size; j++){
-            minimum = min(minimum, vec[j]);
-        }
+    int i = 0;
 
-        vec[i] = minimum;
+    while (i < size) {
+        int minimum = INT16_MAX;  // Reset minimum for each starting position i
+        for (int j = i; j < size; j++) {
+            minimum = min(minimum, vec[j]);  // Find the minimum in the range [i, size)
+        }
+        vec[i] = minimum;  // Replace element at i with the minimum value
         i++;
     }
 
-    for(int i: vec){
-        cout<<i<< " ";
+    for (int i : vec) {
+        cout << i << " ";
     }
     return 0;
 }
