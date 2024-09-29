88af9c55edc0e60bbe3c918a7c24d7f574ee9875
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-09-29 10:09:30 +0530

    Insrtion sorting done

---
+++ Sorting_insertion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> array = {2, 1, 4, 3, 7, 9, 10, 8};
+    int size = array.size();
+
+    for(int i=1; i<size; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            if(array[i]<array[j])
+            {
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+
+    for(int i: array)
+    {
+        cout<< i << " ";
+    }
+    return 0;
+}
