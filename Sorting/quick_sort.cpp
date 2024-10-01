549f55592d9726d917393c2b1b8b7194ed7c7ad7
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-01 07:43:01 +0530

    progress in the algorithm

--- Sorting/quick_sort.cpp
+++ Sorting/quick_sort.cpp
@@ -1,8 +1,52 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 
 using namespace std;
-int main(){
+
+int partition(int arr[], int low, int high)
+{
+    int i = low-1;
+    int pivot = arr[high];
+    for(int j=low; j<high; j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+     i++;
+        int t = arr[i];
+        arr[i] = pivot;
+        pivot = t;
+
+        return i;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low << high)
+    {
+        int pivot = partition(arr,low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+}
+int main()
+{
+    int arr[] = {4, 3, 5, 8, 7, 9, 1, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int low = 0;
+    int high = n - 1;
+
+    quickSort(arr,low,high);
+
+    for(int i=0; i<=high; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
