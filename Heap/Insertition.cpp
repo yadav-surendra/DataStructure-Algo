ffef1a0daf20326e22d0648062129324223d5927
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-01 15:36:26 +0530

    heap insertition code first commit

---
+++ Heap/Insertition.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class heap{
+    int arr[100];
+    int size;
+public:
+    heap(){
+        arr[0] = -1;
+        size = 0;
+    }
+    void insert(int n)
+    {
+        int index = size+1;
+        int parent = index/2;
+    while(index>1)
+    {
+        if(arr[index] > arr[parent])
+        {
+            swap(arr[index],arr[parent]);
+        }
+        else 
+        return;
+    }         
+    }
+
+    void print()
+    {
+        for(int i=1; i<=size; i++)
+        {
+            cout<< arr[i] << " ";
+        }
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(20);
+    h.insert(30);
+    h.insert(15);
+    h.insert(45);
+
+    h.print();
+    return 0;
+}
