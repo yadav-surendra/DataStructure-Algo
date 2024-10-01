b3332efdfc193692742ad20255a943ff593e0e4c
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-01 15:24:04 +0530

    Intro to the Heap

---
+++ Heap/Heap_intro.cpp
@@ -0,0 +1,11 @@
+/*
+what is heap?
+It is a completet Binary tree that comes with a heap order property.
+
+A complete Binary tree is a binary tree in which all the levels are completely filled except posibbly the lowest one, whihch is filled from the left side.
+A complete binary tree is judt like a full binary tree.
+
+# Heap order property:
+It is a way in which the elements are filled in the heap. There are two heap order. One is max-heap and other is min-heap order. In max-heap, the parent is always greter than the child and in min-heap, the parent is always lesser than the child.
+
+*/
