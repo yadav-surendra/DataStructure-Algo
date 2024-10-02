3085d1cd3bd1501a67989e18529ea9a2ee5bf00e
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-02 13:29:49 +0530

    final commit done. very happy feeling.

--- Heap/Heapify_algo.cpp
+++ Heap/Heapify_algo.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 void heapify(int arr[], int n, int i)
 {
+    int largest = i;
     int left = 2*i;
     int right = 2*i+1;
-    int largest = i;
-
-    if(left < n && arr[largest] < arr[left])
+    
+    if(left <= n && arr[largest] < arr[left])
     {
         largest = left;
     }
-    if(right < n && arr[largest] < arr[right])
+    if(right <= n && arr[largest] < arr[right])
     {
         largest = right;
     }
@@ -29,7 +29,7 @@ int main()
     int arr[] = { -1,55,54,52,57,59};
     int n = 5;
 
-    for(int i=n/2; i>=1; i--)
+    for(int i=n/2; i>0; i--)
     {
         heapify(arr,n,i);
     }
