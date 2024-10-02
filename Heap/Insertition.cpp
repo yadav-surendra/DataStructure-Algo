41237083e8d321cf1c83019ccb13670f8eec3992
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-02 07:55:53 +0530

    final commit

--- Heap/Insertition.cpp
+++ Heap/Insertition.cpp
@@ -1,43 +1,48 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm> // Include this for std::swap
 using namespace std;
 
-class heap{
+class heap
+{
     int arr[100];
     int size;
 
 public:
-    heap(){
+    heap()
+    {
         arr[0] = -1;
         size = 0;
     }
+
     void insert(int val)
     {
         size++;
         int index = size;
         arr[index] = val;
 
-    while(index > 1)
-    {
-        int parent = index/2;
-        if(arr[parent] < arr[index])
+        while (index > 1)
         {
-            swap(arr[parent],arr[index]);
-            index = parent;
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]); // Use std::swap here
+                index = parent;
+            }
+            // this segment is important because it prevents the machine from going in infinite loop.
+            else
+            {
+                return;
+            }
         }
-        // else{
-        //     return;
-        // }
-       
-    }         
     }
-    
+
     void print()
     {
-        for(int i=1; i<=size; i++)
+        for (int i = 1; i <= size; i++)
         {
-            cout<< arr[i] << " ";
+            cout << arr[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 };
 
