8dfcade68aa0ab5019ab46f8816bdadb2b01f00a
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-03 11:19:34 +0530

    implemented queue stl

---
+++ Heap/Priority_queue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main()
+{
+    // By default max-heap is formed.
+    priority_queue<int> pq;
+
+    pq.push(5);
+    pq.push(2);
+    pq.push(9);
+    pq.push(1);
+    pq.push(10);
+
+    cout<< pq.top()<<endl;  // prints the max element in the heap as in max-heap top element is the largest.
+    pq.pop();
+    cout<< pq.top()<<endl;
+    cout<<pq.size()<<endl;
+
+    return 0;
+
+}
