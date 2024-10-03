0ba734c817b41dd754aaba0a0bd4e63144433905
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-03 11:26:14 +0530

    min-heap also implemented using queue.

--- Heap/Priority_queue.cpp
+++ Heap/Priority_queue.cpp
@@ -27,6 +27,30 @@ int main()
     }
 
 // Now lets see how we can make min-heap using priority queue stl.
+
+    cout<<"Implememting min-heap using queue."<<endl;
+    
+    priority_queue< int , vector<int> , greater<int> > min_heap;
+
+    min_heap.push(5);
+    min_heap.push(2);
+    min_heap.push(9);
+    min_heap.push(1);
+    min_heap.push(10);
+
+    cout<< min_heap.top()<<endl;  // prints the max element in the heap as in max-heap top element is the largest.
+    min_heap.pop();
+    cout<< min_heap.top()<<endl;
+    cout<<min_heap.size()<<endl;
+
+    if(min_heap.empty())
+    {
+        cout<<"The queue is empty."<<endl;
+    }
+    else{
+        cout<<"THe queue s not empty."<<endl;
+    }
+
     return 0;
 
 }
