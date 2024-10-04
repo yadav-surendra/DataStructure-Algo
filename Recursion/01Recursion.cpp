18e47e2fc34862b4dec037ac29af7bfcd2f4df99
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-04 17:46:31 +0530

    final commit

--- Recursion/01Recursion.cpp
+++ Recursion/01Recursion.cpp
@@ -1,17 +1,17 @@
 // The problem is to print numbers from 5 to 1
-
 #include <iostream>
 using namespace std;
 
-// void printNumFrom5to1(int n)
-// {
-//     if(n>0)
-//     {
-//         cout<<n<<" ";
-//         n--;
-//         printNumFrom5to1(n);
-//     }
-// }
+void printNumFrom5to1(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<" ";
+        n--;
+        printNumFrom5to1(n);
+    }
+}
+
 // to print numbers from one to five.
 void printNumFrom1to5(int m, int n)
 {
