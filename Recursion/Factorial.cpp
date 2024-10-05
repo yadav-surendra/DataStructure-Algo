cbcb3d950beb6f31b7f0727a5fa6e3925b9b4a22
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-05 09:58:17 +0530

    Factorial of n code is done

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int factorial ( int n){
+    if(n==0 || n == 1){
+        return 1;
+    }
+
+    return (n * factorial(n-1));
+}
+
+int main(){
+    int n=3;
+    cout <<"Factoriall of "<< n << " is "<< factorial(n);
+
+    return 0;
+}
