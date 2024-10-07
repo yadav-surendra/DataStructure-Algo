50aac854ae8f5f941b551dbc86820583c7d0ce91
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-07 09:29:06 +0530

    done

---
+++ Recursion/Occurrence_of_element_in_string.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void find_occurrence(string str, int idx, char el){
+    static int start = -1;
+    static int last = -1;
+// Base Case
+    if(idx == str.length())
+    {
+        cout<<"The first occurrence of "<<el << " is "<< start<<endl;
+        if(last != -1)
+        {
+            cout<<"The last occurrence of "<<el << " is  "<<last<<endl;
+        }
+        if(last == -1)
+        {
+            cout<<"The last occurrence of "<<el << " is also "<<start<<endl;
+        }
+        
+        return;
+    }
+// Recursive step
+    char curr_element = str[idx];
+    if(curr_element == el){
+        if(start == -1)
+        {
+            start = idx;
+        }
+        else{
+            last = idx;
+        }
+    }
+    find_occurrence(str, idx+1, el);
+
+}
+
+int main()
+{
+    string s = "surendra";
+    find_occurrence(s,0,'r');
+
+    return 0;
+}
