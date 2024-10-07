6e09f31084e51b93537abc0fd5e3682ff5ab3f3e
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-07 08:57:49 +0530

    code to reverse an string using reursion done successfully passing all test cases.

---
+++ Recursion/Reversing_string.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void reverse(string str, int i)
+{
+    // Base case
+    if(i==0)
+    {
+        cout<<str[i];
+        return;
+    }
+    // Recursive steps
+    cout << str[i];
+    reverse(str,i-1);
+}
+
+int main()
+{
+    string s = "anirudh";
+    int len = s.length();
+    // cout<<len;
+    reverse(s,len-1);
+    return 0;
+}
