e0573afedb59495fbba9cc44ecffd827c855ce41
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-10-21 10:17:32 +0530

    backtracking priblem 1

---
+++ Recursion/Backtracking/printPermuation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+void printPerm(string str, string perm)
+{
+    if(str.length() == 0)
+    {
+        cout<<perm<< endl;
+        return;
+    }
+    for(int i=0; i<str.length(); i++)
+    {
+        char curr_char = str[i];
+        string newstr = str.substr(0,i) + str.substr(i+1);
+        printPerm(newstr, perm + curr_char);
+    }
+    
+}
+
+int main(){
+    
+    string str = "ABC";
+    printPerm(str,"");
+
+    return 0;
+}
