85fdf8abac93224b1537f5dc54ddddce2f371ba8
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-11-14 13:37:08 +0530

    deletion code added

--- Linked List/Day1.cpp
+++ Linked List/Day1.cpp
@@ -13,6 +13,17 @@ class Node {
         this -> data = d;
         this -> next = NULL;
     }
+
+     //destructor
+    ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
     
 };
 
@@ -22,6 +33,78 @@ void insertAtHead(Node* &head , int d)
     Node* temp = new Node(d);
     temp-> next = head;
     head = temp;
+}
+
+void insertAtTail(Node* &tail , int d)
+{
+    // create new node
+    Node* temp = new Node(d);
+    tail-> next = temp;
+    tail = temp;
+}
+
+// insert at any position
+ void insertAtPosition(Node* tail, Node* head, int position, int d)
+ {
+    // insert at start
+    if(position == 1)
+    {
+        insertAtHead(head,d);
+        return;
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+
+    while(cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // inserting at last position
+    if(temp -> next == NULL)
+    {
+        insertAtTail(tail,d);
+        return;
+    }
+
+    // creating a node for d
+    Node* nodeToInsert = new Node(d);
+
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+
+ }
+
+ void deleteNode(int position, Node* & head) { 
+
+    //deleting first or start node
+    if(position == 1) {
+        Node* temp = head;
+        head = head -> next;
+        //memory free start ndoe
+        temp -> next = NULL;
+        delete temp;
+    }
+    else
+    {
+        //deleting any middle node or last node
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int cnt = 1;
+        while(cnt < position) {
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+
+        prev -> next = curr -> next;
+        curr -> next  = NULL;
+        delete curr;
+
+    }
 }
 
 void print(Node* &head) {
@@ -39,6 +122,7 @@ void print(Node* &head) {
     cout << endl;
 }
 
+
 int main()
 {
    
@@ -55,6 +139,8 @@ int main()
     insertAtHead(head,25);
    // cout<<head->data;
 
+    print(head);
+    deleteNode(4,head);
     print(head);
 
     return 0;
