d2bd90bfc26c596f772f611b154cf9f116af759d
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-11-25 19:43:42 +0530

    code to check redundant brackets in an expression

---
+++ Stack_and_Queue/redundant_brackets.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    int checkRedundancy(string s)
+    {
+        // code here
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch = s[i];
+
+            if (ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/')
+            {
+                st.push(ch);
+            }
+            else
+            {
+
+                if (ch == ')')
+                {
+                    bool redundant = true;
+
+                    while (st.top() != '(')
+                    {
+                        char top = st.top();
+
+                        if (top == '+' || top == '-' || top == '*' || top == '/')
+                        {
+                            redundant = false;
+                        }
+                        st.pop();
+                    }
+
+                    if (redundant == true)
+                        return true;
+
+                    st.pop();
+                }
+            }
+        }
+        return false;
+    }
+};
