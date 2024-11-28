4f69752c2b9e15f5f505215d16ba5be95a02541c
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-11-28 07:52:01 +0530

    reversed queue using stack in linera time and space.

---
+++ Stack_and_Queue/queue_reversal_with_stack.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+queue<int> rev(queue<int> q)
+{
+    stack<int> s;
+    
+    while(!q.empty()) {
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+
+    while(!s.empty()) {
+        int element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
